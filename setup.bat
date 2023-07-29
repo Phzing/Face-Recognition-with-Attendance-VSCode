b12f0e9766499d2a2f92aed2f1fdfb97c4e73587
Phzing <hoffman.z@wustl.edu>
2023-07-29 00:19:07 -0700

    updated buggy .bat script

--- setup.bat
+++ setup.bat
@@ -1,10 +1,11 @@
-python3 -m venv myenv
-cd myenv
-source bin/activate
+@echo off
+python -m venv myenv
+cd myenv\Scripts
+call activate
 pip install --upgrade pip
 pip install cmake
 pip install dlib
 pip install face_recognition
 pip install numpy
 pip install opencv-python
-cd ..
+cd ..\..
